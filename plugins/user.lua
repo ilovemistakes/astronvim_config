4a4e01603e35d3263cb3b1cca49a34d3094bc621
ilovemistakes <ilovemistakes4u@gmail.com>
2023-08-25 00:39:14 +0300

    add markdown-preview, vimwiki and surround plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,9 +11,9 @@ return {
   -- },
   {
       "iamcco/markdown-preview.nvim",
-      run = function()
-          vim.fn["mkdp#util#install"]()
-      end,
+      build = "cd app && npm install",
+      enabled = true,
+      ft = "markdown",
   },
   {
      "catppuccin/nvim",
@@ -21,5 +21,23 @@ return {
       config = function()
           require("catppuccin").setup {}
       end,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+  {
+    "ellisonleao/glow.nvim",
+    config = true,
+    cmd = "Glow"
+  },
+  {
+    "vimwiki/vimwiki",
   },
 }
